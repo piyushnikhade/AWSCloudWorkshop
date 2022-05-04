c54d31bc3ec910c703f94b457c57f1881fddc00d
ikramh09 <94330629+ikramh09@users.noreply.github.com>
2022-05-03 22:22:41 -0400

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,40 +1,82 @@
+
 # taking two inputs at a time
 x= int(input("Enter X value: "))
 y= int(input("Enter Y value: "))
+z = int(input("Enter Z value:"))
 print("Value of X= ", x)
 print("Value of Y= ", y)
+print("Value of Z= ", z)
 
 def minus():
     print ("Which Operation you would like to perform")
-    print ("1: X-Y 2: Y-X")
+    print ("1: X-Y 2: Y-X 3: Z-X 4: Z-Y 5: X-Z 6: Y-Z")
     p= int(input("Operation: "))
     if (p==1):
         return x-y
-    else:
+    if (p==2):
         return y-x
+    if (p==3):
+        return z-x
+    if (p==4):
+        return z-y
+    if (p==5):
+        return x-z
+    else:
+        return y-z
 def add():
-    return x+y
+    print ("Which Operation you would like to perform")
+    print("1 X+Y 2: Z+X 3: Z+Y ")
+    p=int(input("Operation: "))
+    if (p==1):
+        return x+y
+    if (p==2):
+        return z+x
+    else:
+        return z+y
 def mult():
-    return x*y
+    print("Which Operation would you like to perform")
+    print("1: X*Y 2: Z*X 3: Z*Y")
+    p=int(input("Operation: "))
+    if (p==1):
+      return x*y
+    if (p==2):
+      return z*x
+    else:
+      return z*y
 def power():
     print ("Which Operation you would like to perform")
-    print ("1: X**Y 2: Y**X")
+    print ("1: X**Y 2: Y**X 3: Z**X 4: Z**Y 5: X**Z 6: Y**Z")
     p= int(input("Operation: "))
     if (p==1):
         return x**y
-    else:
+    if (p==2):
         return y**x
+    if (p==3):
+        return z**x
+    if (p==4):
+        return z**y
+    if (p==5):
+        return x**z
+    else:
+        return y**z
 def divide():
     print ("Which Operation you would like to perform")
-    print ("1: X/Y 2: Y/X")
+    print ("1: X/Y 2: Y/X 3: Z/X 4: Z/Y 5: X/Z 6: Y/Z")
     p= int(input("Operation: "))
     if (p==1):
         return x/y
-    else:
+    if (p==2):
         return y/x
-    
+    if (p==3):
+        return z/x
+    if (p==4):
+        return z/y
+    if (p==5):
+        return x/z
+    else:
+        return y/z
 def default():
-    return x+y
+    x+y+z
 switcher ={
     0:minus,
     1:add,
@@ -47,5 +89,5 @@ def operation(op):
     return switcher.get(op,default)()
 
 print ("0: Minus, 1:Addition, 2:Multiplication, 3:Power, 4:Divide")
-z= int(input("Enter Operation Number: "))
-print (operation(z))
+z1= int(input("Enter Operation Number: "))
+print (operation(z1))
